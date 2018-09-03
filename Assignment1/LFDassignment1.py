1f94d6504c2fb1a363063aaae7c4978c895ae63d
EmilioOldenziel <emilio.oldenziel@gmail.com>
2018-09-03 16:22:28 +0200

    commented assg 1.1

--- Assignment1/LFDassignment1.py
+++ Assignment1/LFDassignment1.py
@@ -4,7 +4,15 @@ from sklearn.pipeline import Pipeline
 
 from sklearn.metrics import accuracy_score
 
-# COMMENT THIS
+# Reads the trainset file using it's filename from the filesystem using utf-8 
+# encoding.
+# The document is read per line and stripped, the words and characters on the 
+# list are placed in a list as tokens using the split() function.
+# Tokens in index 3 and higher are stored as the document.
+# If the use_sentiment variable is True, the sentiment in the form of postitive 
+# and negative is used as labels.
+# If the use_sentiment variable is not True (ELSE), the classes are used as 
+# labels.
 def read_corpus(corpus_file, use_sentiment):
     documents = []
     labels = []
@@ -27,7 +35,9 @@ def read_corpus(corpus_file, use_sentiment):
 def identity(x):
     return x
 
-# COMMENT THIS
+# First, the trainset file is read and the X and Y vectors are set.
+# Second, the X and Y vectors are split in a test and train set using 75% of 
+# the data set as train set and 25% of the data set as test set.
 X, Y = read_corpus('trainset.txt', use_sentiment=True)
 split_point = int(0.75*len(X))
 Xtrain = X[:split_point]
@@ -52,12 +62,19 @@ classifier = Pipeline( [('vec', vec),
                         ('cls', MultinomialNB())] )
 
 
-# COMMENT THIS
+# The classifier consists of a Vectorizer that tranfers the words vector to a 
+# numeric vector that is used by the chosen classifier.
+# The resulting vectors are piped to the Multinomial Naive Bayes classifier, 
+# which receives the trainingset contraining the X and Y vectors.
+# The traingingset is used to fit/train a Naive Bayes model.
 classifier.fit(Xtrain, Ytrain)
 
-# COMMENT THIS  
+# After the classifier's training process has completed, the resulting model is 
+# used to predict the testset's labels.
 Yguess = classifier.predict(Xtest)
 
-# COMMENT THIS
+# To retrieve the classification accuracy, the predicted labels from testset 
+# are compared to the testset's ground truth labels.
+# The precentage of labels that are equal is the resulting accuracy.
 print(accuracy_score(Ytest, Yguess))
 
